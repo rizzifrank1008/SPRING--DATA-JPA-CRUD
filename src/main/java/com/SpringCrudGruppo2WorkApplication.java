36f9eabfdbac5ff208bde344546ee4d9c10677f5
francesco <rizzi4586@gmail.com>
2023-06-05 10:30:54 +0200

    firt commit

---
+++ src/main/java/com/SpringCrudGruppo2WorkApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudGruppo2WorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudGruppo2WorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringCrudGruppo2WorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudGruppo2WorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
