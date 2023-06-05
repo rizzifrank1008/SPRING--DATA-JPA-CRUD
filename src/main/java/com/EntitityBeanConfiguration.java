f10880a283e654f1f252a86af596334540ec5d58
francesco <rizzi4586@gmail.com>
2023-06-05 11:57:42 +0200

    insert entity singola creato ed eseguito

---
+++ src/main/java/com/EntitityBeanConfiguration.java
@@ -0,0 +1,29 @@
+package com;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.entity.Category;
+
+@Configuration
+public class EntitityBeanConfiguration {
+
+	@Bean(name = "categoryToInsert1")
+
+	public Category getCategoryToInsert1() {
+		return new Category("Horror");
+	}
+
+	@Bean(name = "categoryToInsert2")
+
+	public Category getCategoryToInsert2() {
+		return new Category("Fantasy");
+	}
+
+	@Bean(name = "categoryToInsert3")
+
+	public Category getCategoryToInsert3() {
+		return new Category("Comic");
+	}
+
+}
--- src/main/java/com/SpringCrudGruppo2WorkApplication.java
+++ src/main/java/com/SpringCrudGruppo2WorkApplication.java
@@ -1,13 +1,42 @@
 package com;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.entity.Category;
+import com.repository.CategoryRepository;
+
+import jakarta.annotation.Resource;
+
 @SpringBootApplication
-public class SpringCrudGruppo2WorkApplication {
+public class SpringCrudGruppo2WorkApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Resource(name = "categoryToInsert1")
+	private Category categoryToInsert1;
+
+	@Resource(name = "categoryToInsert2")
+	private Category categoryToInsert2;
+
+	@Resource(name = "categoryToInsert3")
+	private Category categoryToInsert3;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCrudGruppo2WorkApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		categoryRepository.save(categoryToInsert1);
+		categoryRepository.save(categoryToInsert2);
+		categoryRepository.save(categoryToInsert3);
+
 	}
 
 }
