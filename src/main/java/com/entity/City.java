4e8d3c8af648a7fdec26717396607cb083fd1ee4
francesco <rizzi4586@gmail.com>
2023-06-05 12:33:09 +0200

    aggiunta entity one to many

---
+++ src/main/java/com/entity/City.java
@@ -0,0 +1,94 @@
+package com.entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQuery;
+
+/**
+ * The persistent class for the city database table.
+ * 
+ */
+@Entity
+@NamedQuery(name = "City.findAll", query = "SELECT c FROM City c")
+public class City implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String city;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "city_id")
+	private int cityId;
+
+	@UpdateTimestamp
+	@Column(name = "last_update")
+	private Timestamp lastUpdate;
+
+	/*
+	 * Stiamo dichiarando un'associazione many-to-one tra l'entità corrente e
+	 * un'altra entità "Country". In particolare, stiamo specificando che l'entità
+	 * corrente, che include questa dichiarazione, ha una relazione many-to-one con
+	 * l'entità "Country", utilizzando la chiave esterna "country_id" come
+	 * riferimento. Questo significa che un'istanza dell'entità corrente può avere
+	 * una sola istanza dell'entità "Country" associata ad essa, ma l'entità
+	 * "Country" può essere associata a molte istanze dell'entità corrente.
+	 */
+	@ManyToOne
+	@JoinColumn(name = "country_id")
+	private Country country;
+
+	public City() {
+	}
+
+	// Costruttore per l'update
+	public City(String city, int cityId) {
+		this.city = city;
+		this.cityId = cityId;
+
+	}
+
+	// Costruttore per l'insert
+	public City(String city) {
+		this.city = city;
+
+	}
+
+	public String getCity() {
+		return this.city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getCityId() {
+		return this.cityId;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public Timestamp getLastUpdate() {
+		return this.lastUpdate;
+	}
+
+	public void setLastUpdate(Timestamp lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+}
---
+++ src/main/java/com/entity/Country.java
@@ -0,0 +1,85 @@
+package com.entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToMany;
+
+@Entity
+@NamedQuery(name = "Country.findAll", query = "SELECT c FROM Country c")
+public class Country implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String country;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "country_id")
+	private int countryId;
+
+	@UpdateTimestamp
+	@Column(name = "last_update")
+	private Timestamp lastUpdate;
+
+	// si mette per indicare chi è l owner della relazione
+
+	@OneToMany(mappedBy = "country", cascade = { CascadeType.PERSIST, CascadeType.REMOVE })
+	private List<City> city;
+
+	public Country() {
+	}
+	// Costruttore per l'insert
+
+	public Country(String country) {
+		this.country = country;
+
+	}
+
+	// Costruttore per l'update
+	public Country(int countryId, String country) {
+		this.countryId = countryId;
+		this.country = country;
+	}
+
+	public String getCountry() {
+		return this.country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getCountryId() {
+		return this.countryId;
+	}
+
+	public void setCountryId(int countryId) {
+		this.countryId = countryId;
+	}
+
+	public Timestamp getLastUpdate() {
+		return this.lastUpdate;
+	}
+
+	public void setLastUpdate(Timestamp lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	public List<City> getCity() {
+		return city;
+	}
+
+	public void setCity(List<City> city) {
+		this.city = city;
+	}
+
+}
